46b681e7dd5881d85cce8559ee41fc1407b52775
consious_coder <varaprasadh.a@gmail.com>
2026-06-23 00:40:03 +0530

    Update publish_next.js

--- publisher/publish_next.js
+++ publisher/publish_next.js
@@ -53,7 +53,7 @@ const carousel = await createCarouselContainer({
   igUserId,
   children: childIds,
   caption: next.caption,
-  collaborators: ['edge_runr.mp4'],
+  collaborators: ['edge_runr'],
   token,
 });
 console.log(`  carousel container: ${carousel.id}`);
